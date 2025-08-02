98d1b8261e752ac81c07737a1384a235c9b36e11
Skguidehelper <skguidehelper@gmail.com>
2025-08-02 14:51:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-BOT_TOKEN = "7573157808:AAGsLtYwPrixlWxWKetmbL6s6xKHiuPp9JE"
+TOKEN = "7573157808:AAGsLtYwPrixlWxWKetmbL6s6xKHiuPp9JE"
 ADMIN_ID = "7790292955"
 
 # ✅ Yeh line add karo:
